a0a4f4ea615a30ce739b96d8d2330ad050dabf5b
Tan Quach <tan.quach@birchwoodlangham.com>
2018-04-29 11:13:08 +0100

    Ubuntu 18.04 and version bump

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Based on the official Ubuntu 16.04, this base image adds additional libraries re
 ## Change Log
 
 2018-03-01: Updated atom to version 1.24.0
+2018-04-28: Ubuntu 18.04 and version bump
--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-ATOM_VERSION=1.24.0
+ATOM_VERSION=1.26.1
 
 docker build -t birchwoodlangham/ubuntu-base-atom:$ATOM_VERSION .
 docker tag birchwoodlangham/ubuntu-base-atom:$ATOM_VERSION birchwoodlangham/ubuntu-base-atom:latest
